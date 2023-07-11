6cc6c4e4b18d3110b058266dd0d65bb67754bf4e
JimmyKimunyi <jimmykimunyi@gmail.com>
2023-07-11 09:02:49 +0300

    updates

---
+++ models/note.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+mongoose.set("strict", true);
+
+const url = process.env.MONGODB_URI;
+
+mongoose
+  .connect(url)
+  .then((result) => {
+    console.log("connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log(console.error(error));
+  });
+
+const noteSchema = new mongoose.Schema({
+  content: {
+    type: String,
+    minLength: 5,
+    require: true,
+  },
+  important: Boolean,
+});
+
+noteSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id;
+    delete returnedObject._id;
+    delete returnedObject.__V;
+  },
+});
+
+module.exports = mongoose.model("Note", noteSchema);
